04b7e2cceb06f0cbfe87e8081bc491fbd98ca3d1
William Petit <william.petit@ptitcloud.fr>
2015-04-26 20:44:43 +0200

    Tested on Debian 8

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 apt-get update
 apt-get -y --no-install-recommends install gdm3 xorg curl build-essential git libc6-dev libx11-dev libxinerama-dev libgconf-2-4
 
+# Fix "version GLIBC_2.14 not found" fix in Debian 7
+#echo "deb http://ftp.fr.debian.org/debian testing main" >> /etc/apt/sources.list
+#apt-get -y update
+#apt-get -t testing install libc6-dev
+
 # Install nodejs + npm
 curl -sL https://deb.nodesource.com/setup | bash -
 apt-get -y install nodejs
@@ -74,7 +79,13 @@ module.exports = function(dependencies) {
   nwm.start(function() {
     var env = JSON.parse(JSON.stringify(process.env));
     env.NODE_ENV='production';
-    var child = child_process.spawn('/usr/bin/npm', ['start'], { cwd: '/opt/snap', env: env });
+    var snap = child_process.spawn('/usr/bin/npm', ['start'], { cwd: '/opt/snap', env: env });
+    snap.once('error', function(err) {
+        console.error(err.stack);
+        process.exit(1);
+    });
+    snap.stderr.pipe(process.stderr);
+    snap.stdout.pipe(process.stdout);
   });
 
 };
